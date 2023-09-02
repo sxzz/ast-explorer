7a52aec7fbceb376326277ec9706c553c975553a
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-03 06:13:24 +0800

    feat: save state to url

--- composables/state.ts
+++ composables/state.ts
@@ -3,10 +3,10 @@ import { type Language } from './language'
 
 const PREFIX = 'ast-explorer:'
 
-export const code = useLocalStorage(`${PREFIX}code`, '')
+export const code = ref('')
 export const ast = shallowRef<any>({})
 export const error = shallowRef<any>()
-export const rawOptions = useLocalStorage<string>(`${PREFIX}options`, '')
+export const rawOptions = ref('')
 
 export const hideEmptyKeys = useLocalStorage(`${PREFIX}hide-empty-keys`, true)
 export const hideLocationData = useLocalStorage(
@@ -26,4 +26,22 @@ export const options = computed(() => {
   } catch {
     return null
   }
+})
+
+const location = useBrowserLocation()
+
+const rawUrlState = atou(location.value.hash!.slice(1))
+if (rawUrlState) {
+  const urlState = JSON.parse(rawUrlState)
+  code.value = urlState.c
+  rawOptions.value = urlState.o
+}
+
+watch([code, rawOptions], () => {
+  location.value.hash = utoa(
+    JSON.stringify({
+      c: code.value,
+      o: rawOptions.value,
+    })
+  )
 })
