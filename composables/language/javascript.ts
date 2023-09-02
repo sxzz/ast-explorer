9d1734e8ddf1bcb258e5783395ca556e000af2a0
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-03 05:48:43 +0800

    fix: catch errors

--- composables/language/javascript.ts
+++ composables/language/javascript.ts
@@ -7,10 +7,13 @@ export const javascript: LanguageOption = {
   label: 'JavaScript',
   icon: 'i-vscode-icons:file-type-js-official',
   language(options: ParserOptions | null | undefined) {
-    const normalizedPlugins = (options?.plugins || []).map((item) =>
-      Array.isArray(item) ? item[0] : item
-    )
-    if (normalizedPlugins.includes('typescript')) return 'typescript'
+    try {
+      const plugins = Array.isArray(options?.plugins) ? options!.plugins : []
+      const normalizedPlugins = plugins.map((item) =>
+        Array.isArray(item) ? item[0] : item
+      )
+      if (normalizedPlugins.includes('typescript')) return 'typescript'
+    } catch {}
     return 'javascript'
   },
   options: {
