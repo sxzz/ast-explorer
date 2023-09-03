170ce93afd1e7992727f7d86a53d2624b91a2fd9
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-03 17:49:05 +0800

    feat: add github link

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,11 +7,19 @@
       <ParserOptions v-if="currentParser.options.configurable" />
     </div>
 
-    <div flex="~ gap2">
+    <div flex="~ gap-3">
       <span font-mono text-gray>{{ currentParser.version }}</span>
       <button @click="toggleDark">
         <div i-ri:sun-line dark:i-ri:moon-line />
       </button>
+      <a
+        href="https://github.com/sxzz/ast-explorer"
+        target="_blank"
+        flex
+        items-center
+      >
+        <div i-ri:github-line />
+      </a>
     </div>
   </div>
 </template>
