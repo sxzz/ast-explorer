0db9442148d22a80363a54c0143d0b6aeb17f0da
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-03 19:51:59 +0800

    refactor: simplify syntax

--- composables/language/javascript.ts
+++ composables/language/javascript.ts
@@ -4,6 +4,7 @@ import { type LanguageOption, type Parser } from '../language'
 import type * as Babel from '@babel/parser'
 import type * as Swc from '@swc/wasm-web'
 import type * as Acorn from 'acorn'
+
 // @unocss-include
 
 const babel: Parser<typeof Babel, Babel.ParserOptions> = {
@@ -13,15 +14,15 @@ const babel: Parser<typeof Babel, Babel.ParserOptions> = {
   version: `@babel/parser@latest`,
   options: {
     configurable: true,
-    defaultValue: {},
+    defaultValue: {
+      sourceType: 'module',
+    },
     editorLanguage: 'json',
   },
-  init() {
-    // @ts-expect-error
-    return import('https://cdn.jsdelivr.net/npm/@babel/parser/+esm')
-  },
+  // @ts-expect-error
+  init: () => import('https://cdn.jsdelivr.net/npm/@babel/parser/+esm'),
   parse(code, options) {
-    return this.parse(code, { sourceType: 'module', ...options })
+    return this.parse(code, { ...options })
   },
   editorLanguage(options) {
     try {
@@ -45,14 +46,14 @@ const swc: Parser<typeof Swc, Swc.ParseOptions> = {
     defaultValue: { syntax: 'ecmascript' },
     editorLanguage: 'json',
   },
-  async init() {
-    const mod: typeof Swc = await import(
+  init: () =>
+    import(
       // @ts-expect-error
       'https://cdn.jsdelivr.net/npm/@swc/wasm-web@1.3.82/wasm-web.js'
-    )
-    await mod.default()
-    return mod
-  },
+    ).then(async (mod: typeof Swc) => {
+      await mod.default()
+      return mod
+    }),
   parse(code, options) {
     return this.parse(code, { ...(options as any) })
   },
@@ -71,10 +72,8 @@ const acorn: Parser<typeof Acorn, Acorn.Options> = {
     defaultValue: { ecmaVersion: 'latest', sourceType: 'module' },
     editorLanguage: 'json',
   },
-  init() {
-    // @ts-expect-error
-    return import('https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs')
-  },
+  // @ts-expect-error
+  init: () => import('https://cdn.jsdelivr.net/npm/acorn/dist/acorn.mjs'),
   parse(code, options) {
     return this.parse(code, { ...options })
   },
