5302dad81d86358d0327e1bcc3df50e374e7454e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-04 15:33:09 +0800

    feat: display version

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import { version } from '../package.json'
+</script>
+
 <template>
   <div flex="~ wrap" items-center justify-between p2>
     <div flex="~ gap4">
-      <h1 text-lg font-bold>AST Explorer</h1>
+      <div flex="~ gap1">
+        <h1 text-lg font-bold>AST Explorer</h1>
+        <small>v{{ version }}</small>
+      </div>
       <LanguageSelect />
       <ParserSelect />
       <ParserOptions v-if="currentParser.options.configurable" />
