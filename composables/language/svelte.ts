ad74284ddd05d0de0557bf5e6ee3c7f9dcdc9448
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-09 14:56:26 +0800

    fix

--- composables/language/svelte.ts
+++ composables/language/svelte.ts
@@ -1,8 +1,10 @@
 import { type CompileOptions, type compile } from 'svelte/compiler'
 import { type LanguageOption, type Parser } from '../language'
 
-const Svelte: Parser<{ default: typeof compile }, CompileOptions> = {
-  id: 'Svelte',
+// @unocss-include
+
+const svelteCompiler: Parser<typeof compile, CompileOptions> = {
+  id: 'svelte-compiler',
   label: 'svelte/compiler',
   icon: 'i-vscode-icons:file-type-svelte',
   editorLanguage: 'svelte',
@@ -14,19 +16,22 @@ const Svelte: Parser<{ default: typeof compile }, CompileOptions> = {
     },
     editorLanguage: 'json',
   },
-  // @ts-expect-error
-  init: () => import('https://esm.sh/svelte/src/compiler/compile/index.js'),
+  init: () =>
+    // @ts-expect-error
+    import('https://esm.sh/svelte/src/compiler/compile/index.js').then(
+      (mod) => mod.default
+    ),
   version: () =>
     fetch('https://esm.sh/svelte/package.json')
       .then((r) => r.json())
       .then((raw) => `@svelte/compiler@${raw.version}`),
   parse(code, options) {
-    return this.default(code, options)?.ast
+    return this(code, options)?.ast
   },
 }
 
 export const svelte: LanguageOption = {
   label: 'Svelte',
   icon: 'i-vscode-icons:file-type-svelte',
-  parsers: [Svelte],
+  parsers: [svelteCompiler],
 }
