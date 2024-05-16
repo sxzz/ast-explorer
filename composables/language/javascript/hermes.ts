687a5d5c332789191909ed0c1b5520b224fb25df
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-05-16 12:42:44 +0800

    feat: hermes-parser

---
+++ composables/language/javascript/hermes.ts
@@ -0,0 +1,22 @@
+import type { Parser } from '..'
+
+export const hermes: Parser<any, any> = {
+  id: 'hermes',
+  label: 'Hermes',
+  icon: 'https://github.com/facebook/hermes/raw/main/doc/img/logo.svg',
+  link: 'https://github.com/facebook/hermes',
+  options: {
+    configurable: true,
+    defaultValue: {},
+    editorLanguage: 'json',
+  },
+  pkgName: 'hermes-parser',
+  init: (pkg) =>
+    importUrl(`https://esm.sh/${pkg}`, true).then((mod) => mod.default),
+  version: fetchVersion,
+  parse(code, options) {
+    return this.parse(code, { ...options })
+  },
+  editorLanguage: 'javascript',
+  getAstLocation: getAstLocation('ranges'),
+}
--- composables/language/javascript/index.ts
+++ composables/language/javascript/index.ts
@@ -5,11 +5,12 @@ import { acorn } from './acorn'
 import { espree, tsEslint } from './eslint'
 import { typescript } from './typescript'
 import { flow } from './flow'
+import { hermes } from './hermes'
 import type { LanguageOption } from '..'
 
 export const javascript: LanguageOption = {
   label: 'JavaScript',
   // @unocss-include
   icon: 'i-vscode-icons:file-type-js-official',
-  parsers: [babel, swc, oxc, acorn, typescript, espree, tsEslint, flow],
+  parsers: [babel, swc, oxc, acorn, typescript, espree, tsEslint, flow, hermes],
 }
