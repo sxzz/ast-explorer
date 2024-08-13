b7b5f5fddaea2d49f9396d88e320551b8cf03e15
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-13 22:53:46 +0800

    chore: add ignore build script

---
+++ scripts/ignore-build.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+import { execSync } from 'node:child_process'
+import process from 'node:process'
+
+const gitLog = execSync('git log -1 --pretty=%B', { encoding: 'utf8' }).trim()
+const gitBranch = execSync('git branch --show-current', {
+  encoding: 'utf8',
+}).trim()
+
+if (gitLog.startsWith('chore: release') && gitBranch !== 'release') {
+  console.log('Release commit, skipped.')
+  process.exit(0)
+}
+
+console.log('Proceeding with build.')
+process.exit(1)
