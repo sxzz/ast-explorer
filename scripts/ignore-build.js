c9ce9666d3f69b5e0f99fa07367d83971c548b06
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-13 22:55:17 +0800

    chore: add skip build check

--- scripts/ignore-build.js
+++ scripts/ignore-build.js
@@ -13,5 +13,10 @@ if (gitLog.startsWith('chore: release') && gitBranch !== 'release') {
   process.exit(0)
 }
 
+if (gitLog.includes('skip build')) {
+  console.log('Skip build commit, skipped.')
+  process.exit(0)
+}
+
 console.log('Proceeding with build.')
 process.exit(1)
