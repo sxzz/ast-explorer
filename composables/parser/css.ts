73c86885a3169e5d437767c04bbfde1ad8960cce
翠 / green <green@sapphi.red>
2024-12-26 22:30:27 +0900

    feat: add lightningcss (#110)

--- composables/parser/css.ts
+++ composables/parser/css.ts
@@ -1,6 +1,7 @@
 import { cssTemplate } from './template'
 import type { LanguageOption, Parser } from './index'
 import type * as CssTree from 'css-tree'
+import type * as Lightningcss from 'lightningcss-wasm'
 import type * as Postcss from 'postcss'
 
 // @unocss-include
@@ -52,9 +53,46 @@ const postcss: Parser<typeof Postcss, Postcss.ProcessOptions> = {
   ignoreFocusFields: ['parent'],
 }
 
+const lightningcss: Parser<
+  typeof Lightningcss,
+  Omit<Lightningcss.TransformOptions<{}>, 'code'>
+> = {
+  id: 'lightningcss',
+  label: 'Lightning CSS',
+  icon: 'https://raw.githubusercontent.com/parcel-bundler/lightningcss/7f290350ed0cc53b1267d6810417f0611135eeee/website/favicon.svg',
+  link: 'https://lightningcss.dev/',
+  editorLanguage: 'css',
+  options: {
+    configurable: true,
+    defaultValue: {
+      filename: 'input.css',
+    },
+    editorLanguage: 'javascript',
+    defaultValueType: 'json5',
+  },
+  pkgName: 'lightningcss-wasm',
+  getModuleUrl: (pkgName) => `https://esm.sh/${pkgName}`,
+  async parse(code, options) {
+    await this.default()
+
+    const encoder = new TextEncoder()
+    let result!: Lightningcss.StyleSheet
+    this.transform({
+      ...options,
+      code: encoder.encode(code),
+      visitor: {
+        StyleSheet(stylesheet) {
+          result = stylesheet
+        },
+      },
+    })
+    return result
+  },
+}
+
 export const css: LanguageOption = {
   label: 'CSS',
   icon: 'i-vscode-icons:file-type-css',
-  parsers: [cssTree, postcss],
+  parsers: [cssTree, postcss, lightningcss],
   codeTemplate: cssTemplate,
 }
