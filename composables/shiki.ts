015b593538f7d74efb6de84e2972ee841c45e79e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-02 23:03:22 +0800

    feat: add yaml highlight

--- composables/shiki.ts
+++ composables/shiki.ts
@@ -4,10 +4,18 @@ import langHtml from 'shiki/langs/html.mjs'
 import langJson from 'shiki/langs/json.mjs'
 import langTs from 'shiki/langs/typescript.mjs'
 import langVue from 'shiki/langs/vue.mjs'
+import langYaml from 'shiki/langs/yaml.mjs'
 import vitesseDark from 'shiki/themes/vitesse-dark.mjs'
 import vitesseLight from 'shiki/themes/vitesse-light.mjs'
 
-export const shikiLangs = [langTs, langVue, langJson, langHtml, langCss]
+export const shikiLangs = [
+  langTs,
+  langVue,
+  langJson,
+  langHtml,
+  langCss,
+  langYaml,
+]
 export const highlighter = createHighlighterCoreSync({
   langs: shikiLangs,
   themes: [vitesseLight, vitesseDark],
