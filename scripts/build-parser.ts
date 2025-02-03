9f8023d4a061295ac65773e90189bde25610bf56
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-03 21:32:00 +0800

    refactor: switch to rolldown for pre-bundling typescript-eslint-parser

--- scripts/build-parser.ts
+++ scripts/build-parser.ts
@@ -1,17 +1,16 @@
 import { mkdir, readFile, writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import process from 'node:process'
-import { fileURLToPath } from 'node:url'
-import { version } from '@typescript-eslint/parser'
-import { build } from 'esbuild'
-import Replace from 'unplugin-replace/esbuild'
+import { version } from '@typescript-eslint/parser/package.json'
+import { build } from 'rolldown'
+import Replace from 'unplugin-replace/rolldown'
 import type { ConsolaInstance } from 'consola'
 
-const root = path.resolve(fileURLToPath(import.meta.url), '../..')
+const root = path.resolve(import.meta.dirname, '..')
 
 export async function buildTsEslint(logger: ConsolaInstance, noCache = false) {
   const cacheDir = path.resolve(root, `.nuxt/cache`)
-  await mkdir(cacheDir, { recursive: true }).catch(() => null)
+  await mkdir(cacheDir, { recursive: true })
   const cachePath = path.resolve(cacheDir, `ts-eslint-parser@${version}.js`)
   if (!noCache) {
     const cache = await readFile(cachePath, 'utf8').catch(() => null)
@@ -20,34 +19,36 @@ export async function buildTsEslint(logger: ConsolaInstance, noCache = false) {
       return cache
     }
   }
+  const t = performance.now()
   logger.start('Building @typescript-eslint/parser')
-  const result = await build({
-    stdin: {
-      contents: `export { version, parse } from '@typescript-eslint/parser'`,
-      resolveDir: process.cwd(),
-    },
-    format: 'esm',
+  const ENTRY = 'virtual:entry'
+  const output = await build({
+    input: [ENTRY],
     write: false,
-    bundle: true,
     platform: 'browser',
-    minify: false,
-    alias: {
-      path: 'pathe',
-      'node:path': 'pathe',
-      fs: 'unenv/runtime/mock/proxy',
-      'node:fs': 'unenv/runtime/mock/proxy',
-      os: 'unenv/runtime/mock/proxy',
-      'node:os': 'unenv/runtime/mock/proxy',
-      'node:process': 'unenv/runtime/mock/proxy',
-      util: 'unenv/runtime/mock/proxy',
-      'node:util': 'unenv/runtime/mock/proxy',
-      stream: 'unenv/runtime/mock/proxy',
-      'fast-glob': 'unenv/runtime/mock/proxy',
-      perf_hooks: 'unenv/runtime/mock/proxy',
-      inspector: 'unenv/runtime/mock/proxy',
-      crypto: 'unenv/runtime/mock/proxy',
+    resolve: {
+      /// keep-sorted
+      alias: {
+        '@typescript-eslint/scope-manager': 'unenv/runtime/mock/proxy',
+        'fast-glob': 'unenv/runtime/mock/proxy',
+        'is-glob': 'unenv/runtime/mock/proxy',
+        'node:fs': 'unenv/runtime/mock/proxy',
+        'node:path': 'pathe',
+        'node:util': 'unenv/runtime/mock/proxy',
+        minimatch: 'unenv/runtime/mock/proxy',
+      },
     },
     plugins: [
+      {
+        name: ENTRY,
+        resolveId(id) {
+          if (id === ENTRY) return id
+        },
+        load: (id) => {
+          if (id === ENTRY)
+            return `export { parse, version } from '@typescript-eslint/parser'`
+        },
+      },
       Replace({
         exclude: [],
         values: [
@@ -62,9 +63,14 @@ export async function buildTsEslint(logger: ConsolaInstance, noCache = false) {
         ],
       }),
     ],
+    output: {
+      format: 'esm',
+    },
   })
-  const text = result.outputFiles[0].text
+  const text = output.output[0].code
   await writeFile(cachePath, text, 'utf8')
-  logger.success('Built @typescript-eslint/parser')
+  logger.success(
+    `Built @typescript-eslint/parser in ${Math.round(performance.now() - t)}ms`,
+  )
   return text
 }
