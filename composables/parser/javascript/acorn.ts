29e6fdc953929b38cd965595db6dfb34d77ec482
fisker <lionkay@gmail.com>
2025-02-18 03:08:31 +0800

    Add `comments` option to `acorn` parser

--- composables/parser/javascript/acorn.ts
+++ composables/parser/javascript/acorn.ts
@@ -1,7 +1,15 @@
 import type { Parser } from '..'
 import type * as Acorn from 'acorn'
 
-export const acorn: Parser<typeof Acorn, Acorn.Options> = {
+export type Options = Omit<Acorn.Options, 'onComment'> & {
+  comments?: boolean
+}
+
+type ProgramWithComments = Acorn.Program & {
+  comments?: Acorn.Comment[]
+}
+
+export const acorn: Parser<typeof Acorn, Options> = {
   id: 'acorn',
   label: 'Acorn',
   icon: 'https://cdn.jsdelivr.net/gh/acornjs/acorn@master/logo.svg',
@@ -20,7 +28,16 @@ export const acorn: Parser<typeof Acorn, Acorn.Options> = {
     return (await this).version
   },
   parse(code, options) {
-    return this.parse(code, { ...options })
+    const comments = options.comments ? [] : undefined
+    const ast = this.parse(code, {
+      ...options,
+      onComment: comments,
+    }) as ProgramWithComments
+    if (options.comments) {
+      ast.comments = comments
+    }
+
+    return ast
   },
   editorLanguage: 'javascript',
   getAstLocation,
