e694f4c282fe52d0ab88fc22483ec2eaef26319a
fisker Cheung <lionkay@gmail.com>
2025-02-26 01:47:21 +0800

    feat: add momoa parser (#135)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

---
+++ composables/parser/json/momoa.ts
@@ -0,0 +1,29 @@
+import type { Parser } from '../index'
+import type * as Momoa from '@humanwhocodes/momoa'
+
+// @unocss-include
+
+export const momoa: Parser<typeof Momoa, Momoa.ParseOptions> = {
+  id: 'momoa',
+  label: 'momoa',
+  icon: 'i-vscode-icons:file-type-json',
+  link: 'https://github.com/humanwhocodes/momoa',
+  editorLanguage: 'json',
+  options: {
+    configurable: true,
+    defaultValue: {
+      mode: 'json',
+      ranges: false,
+      tokens: false,
+      allowTrailingCommas: false,
+    },
+    editorLanguage: 'json',
+  },
+  pkgName: '@humanwhocodes/momoa',
+  init: (url) => importUrl(url),
+  parse(code, options) {
+    return this.parse(code, { ...options })
+  },
+  getNodeLocation: genGetNodeLocation('locOffset'),
+  gui: () => import('./MomoaGui.vue'),
+}
