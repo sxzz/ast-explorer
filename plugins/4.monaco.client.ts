450fb6077e1eb3dbf9f60cd4e7385705e21ef459
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-11 19:09:49 +0800

    fix: no tsx error in editor
    closes #142

--- plugins/4.monaco.client.ts
+++ plugins/4.monaco.client.ts
@@ -8,12 +8,19 @@ export default defineNuxtPlugin(() => {
   })
 
   monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+    allowJs: true,
     target: monaco.languages.typescript.ScriptTarget.ESNext,
     module: monaco.languages.typescript.ModuleKind.ESNext,
     allowNonTsExtensions: true,
     moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
     noEmit: true,
     esModuleInterop: true,
-    jsx: monaco.languages.typescript.JsxEmit.Preserve,
+    jsx: monaco.languages.typescript.JsxEmit.React,
+  })
+
+  monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: true,
+    noSyntaxValidation: true,
+    noSuggestionDiagnostics: true,
   })
 })
