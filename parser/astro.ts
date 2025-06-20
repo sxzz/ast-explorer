a938c6b6539deb89802cf6d76affa8f1d6476e48
yvbopeng <53609553+yvbopeng@users.noreply.github.com>
2025-06-20 15:59:55 +0800

    feat: add astro parser (#173)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

---
+++ parser/astro.ts
@@ -0,0 +1,37 @@
+import { astroTemplate } from './template'
+import type { LanguageOption, Parser } from './index'
+import type * as Astro from '@astrojs/compiler/types'
+
+// @unocss-include
+
+const astroCompiler: Parser<typeof Astro, Astro.ParseOptions> = {
+  id: 'astro-compiler',
+  label: '@astrojs/compiler',
+  icon: 'i-vscode-icons:file-type-light-astro',
+  link: 'https://github.com/withastro/compiler',
+  editorLanguage: 'astro',
+  options: {
+    configurable: true,
+    defaultValue: {},
+    editorLanguage: 'json',
+  },
+  pkgName: '@astrojs/compiler',
+  getModuleUrl: (pkg) => `https://esm.sh/${pkg}/es2022/compiler.mjs`,
+  async init(moduleUrl) {
+    const mod = await importUrl<typeof Astro>(moduleUrl)
+    const wasmURL = getJsdelivrUrl('@astrojs/compiler', '/dist/astro.wasm')
+    await mod.initialize({ wasmURL })
+    return mod
+  },
+  async parse(code, options) {
+    return (await this.parse(code, options)).ast
+  },
+  getNodeLocation: genGetNodeLocation('positionOffset'),
+}
+
+export const astro: LanguageOption = {
+  label: 'Astro',
+  icon: 'i-vscode-icons:file-type-light-astro',
+  parsers: [astroCompiler],
+  codeTemplate: astroTemplate,
+}
