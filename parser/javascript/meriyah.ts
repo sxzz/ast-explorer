9ec0231088e22a83b3b65130b4c6c34f4469687c
fisker <lionkay@gmail.com>
2025-06-22 06:34:32 +0800

    feat: support `raw` and `lexical` in Meriyah parser

--- parser/javascript/meriyah.ts
+++ parser/javascript/meriyah.ts
@@ -21,6 +21,7 @@ export const meriyah: Parser<typeof Meriyah, Options> = {
     defaultValue: {
       module: true,
       ranges: true,
+      raw: true,
     },
     editorLanguage: 'json',
   },
