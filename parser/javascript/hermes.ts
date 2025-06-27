4f3509b995e3a3e4b91dc5f9fb6420d37d44b169
fisker <lionkay@gmail.com>
2025-06-27 10:22:30 +0800

    feat: add gui for `Hermes`

--- parser/javascript/hermes.ts
+++ parser/javascript/hermes.ts
@@ -1,21 +1,43 @@
 import type { Parser } from '..'
 
-export const hermes: Parser<any, any> = {
+// https://www.npmjs.com/package/hermes-parser#API
+export type Options = {
+  babel: boolean
+  allowReturnOutsideFunction: boolean
+  flow: 'all' | 'detect'
+  sourceType: 'module' | 'script' | 'unambiguous'
+  tokens: boolean
+  // Undocumented
+  enableExperimentalComponentSyntax: boolean
+  // Undocumented
+  enableExperimentalFlowMatchSyntax: boolean
+}
+
+export const hermes: Parser<any, Options> = {
   id: 'hermes',
   label: 'Hermes',
   icon: 'https://cdn.jsdelivr.net/gh/facebook/hermes@main/doc/img/logo.svg',
   link: 'https://github.com/facebook/hermes',
   options: {
     configurable: true,
-    defaultValue: {},
+    defaultValue: {
+      babel: false,
+      allowReturnOutsideFunction: true,
+      flow: 'all',
+      sourceType: 'unambiguous',
+      tokens: false,
+      enableExperimentalComponentSyntax: true,
+      enableExperimentalFlowMatchSyntax: true,
+    },
     editorLanguage: 'json',
   },
   pkgName: 'hermes-parser',
   getModuleUrl: (pkg) => `https://esm.sh/${pkg}`,
   init: (url) => resolveDefault(importUrl(url, true)),
   parse(code, options) {
-    return this.parse(code, { ...options })
+    return this.parse(code, options)
   },
   editorLanguage: 'javascript',
   getNodeLocation: genGetNodeLocation('range'),
+  gui: () => import('./HermesGui.vue'),
 }
