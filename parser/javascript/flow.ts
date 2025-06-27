6df2596ae8eb13dd841ae1c98275eb6fdb1de8a9
fisker Cheung <lionkay@gmail.com>
2025-06-28 01:04:47 +0800

    feat: add gui for flow parser (#178)
    Co-authored-by: Kevin Deng <sxzz@sxzz.moe>

--- parser/javascript/flow.ts
+++ parser/javascript/flow.ts
@@ -1,6 +1,22 @@
 import type { Parser } from '..'
 
-export const flow: Parser<any, any> = {
+// https://github.com/facebook/flow/tree/main/packages/flow-parser#options
+export interface Options {
+  all_comments?: boolean
+  comments?: boolean
+  enums: boolean
+  esproposal_decorators: boolean
+  esproposal_export_star_as: boolean
+  tokens?: boolean
+  types: boolean
+  use_strict?: boolean
+  // Undocumented
+  components: boolean
+  // Undocumented
+  pattern_matching: boolean
+}
+
+export const flow: Parser<any, Options> = {
   id: 'flow',
   label: 'Flow',
   // @unocss-include
@@ -10,8 +26,11 @@ export const flow: Parser<any, any> = {
     configurable: true,
     defaultValue: {
       enums: true,
+      esproposal_decorators: true,
+      esproposal_export_star_as: true,
       types: true,
       components: true,
+      pattern_matching: true,
     },
     editorLanguage: 'json',
   },
@@ -23,4 +42,5 @@ export const flow: Parser<any, any> = {
   },
   editorLanguage: 'javascript',
   getNodeLocation: genGetNodeLocation('range'),
+  gui: () => import('./FlowGui.vue'),
 }
