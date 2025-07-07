e9c8eeaed15852cffac79b6121f43c29ed9ad1cd
Triumph-light <2495685883@qq.com>
2025-07-07 11:26:42 +0800

    fix: add root div in order to resolve Runtime directive used on component with non-element root node

--- components/tabs/Tab.vue
+++ components/tabs/Tab.vue
@@ -12,23 +12,25 @@ provide('activeTab', innerActiveTab)
 </script>
 
 <template>
-  <div class="w-full flex border-b">
-    <button
-      v-for="tab in tabs"
-      :key="tab.value"
-      :class="[
-        'px-4 py-2 text-sm font-medium',
-        innerActiveTab === tab.value
-          ? 'text-emerald-600 border-b-2 border-emerald-600'
-          : 'text-gray-500 hover:text-emerald-500',
-      ]"
-      @click="innerActiveTab = tab.value"
-    >
-      {{ tab.label }}
-    </button>
-  </div>
-
   <div w-full>
-    <slot />
+    <div class="w-full flex border-b">
+      <button
+        v-for="tab in tabs"
+        :key="tab.value"
+        :class="[
+          'px-4 py-2 text-sm font-medium',
+          innerActiveTab === tab.value
+            ? 'text-emerald-600 border-b-2 border-emerald-600'
+            : 'text-gray-500 hover:text-emerald-500',
+        ]"
+        @click="innerActiveTab = tab.value"
+      >
+        {{ tab.label }}
+      </button>
+    </div>
+
+    <div w-full>
+      <slot />
+    </div>
   </div>
 </template>
