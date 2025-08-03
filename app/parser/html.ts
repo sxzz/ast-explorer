443f58b59e586f236755051ef31cabf73b68b4ed
Alex Liu <dsa1314@gmail.com>
2025-08-03 13:19:00 +0800

    feat: add `ultrahtml`

--- app/parser/html.ts
+++ app/parser/html.ts
@@ -3,6 +3,7 @@ import type { LanguageOption, Parser } from './index'
 import type * as HtmlEslintParser from '@html-eslint/parser'
 import type * as Htmlparser2 from 'htmlparser2'
 import type * as Rehype from 'rehype'
+import type * as Ultrahtml from 'ultrahtml'
 
 // @unocss-include
 
@@ -65,9 +66,28 @@ const htmlEslintParser: Parser<
   getNodeLocation: genGetNodeLocation('range'),
 }
 
+const ultrahtmlParser: Parser<typeof Ultrahtml> = {
+  id: 'ultrahtml-parser',
+  label: 'ultrahtml',
+  icon: 'i-vscode-icons:file-type-html',
+  link: 'https://github.com/natemoo-re/ultrahtml/',
+  editorLanguage: 'html',
+  options: {
+    configurable: false,
+    defaultValue: '{}',
+    editorLanguage: 'javascript',
+  },
+  pkgName: 'ultrahtml',
+  parse(code) {
+    return this.parse(code)
+  },
+  getNodeLocation: genGetNodeLocation('ultrahtml'),
+  hideKeys: ['parent'],
+}
+
 export const html: LanguageOption = {
   label: 'HTML',
   icon: 'i-vscode-icons:file-type-html',
-  parsers: [htmlparser2, rehypeAst, htmlEslintParser],
+  parsers: [htmlparser2, rehypeAst, htmlEslintParser, ultrahtmlParser],
   codeTemplate: htmlTemplate,
 }
