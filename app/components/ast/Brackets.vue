1c99e1efe4712f84d984d218c1ccc44874980df6
Alex Liu <dsa1314@gmail.com>
2025-08-03 22:28:34 +0800

    refactor: improve bracket spacing in AST display (#191)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

--- app/components/ast/Brackets.vue
+++ app/components/ast/Brackets.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-const props = defineProps<{ data: any }>()
+const props = defineProps<{
+  data: any
+  open?: boolean
+}>()
 const start = computed(() => (Array.isArray(props.data) ? '[' : '{'))
 const end = computed(() => (Array.isArray(props.data) ? ']' : '}'))
 </script>
 
 <template>
-  <span op70>{{ start }}&nbsp;</span>
+  <span op70>{{ start }}{{ open ? '' : '&nbsp;' }}</span>
   <slot />
-  <span op70>&nbsp;{{ end }}</span>
+  <span op70>{{ open ? '' : '&nbsp;' }}{{ end }}</span>
 </template>
