28325cdd8a60fe2a21cbaa9510bf6075de0f0b73
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-03 22:35:48 +0800

    chore: upgrade deps

--- app/state/theme.ts
+++ app/state/theme.ts
@@ -34,7 +34,7 @@ export function toggleDark(event?: MouseEvent) {
     ]
     document.documentElement.animate(
       {
-        clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
+        clipPath: isDark.value ? clipPath.toReversed() : clipPath,
       },
       {
         duration: 400,
