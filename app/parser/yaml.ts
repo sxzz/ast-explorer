4dfb47b3caad94f04f4d97dc57f612007b799756
fisker <lionkay@gmail.com>
2025-12-05 18:23:51 +0800

    Add `yaml-unist-parser`

--- app/parser/yaml.ts
+++ app/parser/yaml.ts
@@ -2,6 +2,7 @@ import { yamlTemplate } from './template'
 import type { LanguageOption, Parser } from './index'
 import type * as Yaml from 'yaml'
 import type * as YamlEslintParser from 'yaml-eslint-parser'
+import type * as YamlUnistParser from 'yaml-unist-parser'
 
 const yamlParser: Parser<
   typeof Yaml,
@@ -44,10 +45,35 @@ const yamlEslintParser: Parser<typeof YamlEslintParser, any> = {
   hideKeys: ['parent'],
 }
 
+const yamlUnistParser: Parser<
+  typeof YamlUnistParser,
+  YamlUnistParser.ParseOptions
+> = {
+  id: 'yaml-unist-parser',
+  label: 'yaml-unist-parser',
+  icon: 'i-vscode-icons:file-type-yaml',
+  link: 'https://github.com/prettier/yaml-unist-parser',
+  editorLanguage: 'yaml',
+  options: {
+    configurable: false,
+    defaultValue: {
+      uniqueKeys: false,
+    },
+    editorLanguage: 'json',
+  },
+  pkgName: 'yaml-unist-parser',
+  getModuleUrl: (pkg) => `https://esm.sh/${pkg}`,
+  parse(code, options) {
+    return this.parse(code, options)
+  },
+  getNodeLocation: genGetNodeLocation('range'),
+  hideKeys: ['parent'],
+}
+
 export const yaml: LanguageOption = {
   label: 'YAML',
   // @unocss-include
   icon: 'i-vscode-icons:file-type-yaml',
-  parsers: [yamlParser, yamlEslintParser],
+  parsers: [yamlParser, yamlEslintParser, yamlUnistParser],
   codeTemplate: yamlTemplate,
 }
