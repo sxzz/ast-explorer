a0851a355fe1bbd463267617c6a9bf7567af0579
Kevin Deng <sxzz@sxzz.moe>
2026-02-16 16:08:56 +0800

    fix: csharp icon

--- app/parser/csharp.ts
+++ app/parser/csharp.ts
@@ -3,6 +3,7 @@ import type { LanguageOption } from './index'
 
 export const csharp: LanguageOption = {
   label: 'C#',
+  // @unocss-include
   icon: 'i-vscode-icons:file-type-csharp',
   parsers: [treeSitterCSharp],
   codeTemplate: csharpTemplate,
