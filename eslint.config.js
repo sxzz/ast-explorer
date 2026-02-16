d25b24a24220e2165009c4abdb05b03017092e7d
Kevin Deng <sxzz@sxzz.moe>
2026-02-16 16:07:50 +0800

    chore: upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,18 @@
 import { sxzz } from '@sxzz/eslint-config'
 
-export default sxzz({ pnpm: true })
-  .removeRules('node/no-unsupported-features/es-builtins', 'import/first')
+export default sxzz({
+  vue: true,
+  pnpm: true,
+  baseline: {
+    ignoreFeatures: ['top-level-await'],
+  },
+})
+  .append({
+    files: ['**/*.md/**'],
+    rules: {
+      'import/first': 'off',
+    },
+  })
   .append({
     ignores: ['app/parser/template/**'],
   })
