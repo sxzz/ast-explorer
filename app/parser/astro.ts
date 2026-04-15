d47396f8f054a8f69edb7547c70ea3f541531cb1
knj <kenji.tomita1996@gmail.com>
2026-04-15 14:18:14 +0900

    feat: add @astrojs/compiler-rs parser (#261)
    Co-authored-by: Kevin Deng <sxzz@sxzz.moe>

--- app/parser/astro.ts
+++ app/parser/astro.ts
@@ -1,5 +1,6 @@
 import { astroTemplate } from './template'
 import type { LanguageOption, Parser } from './index'
+import type * as AstroRs from '@astrojs/compiler-binding'
 import type * as Astro from '@astrojs/compiler/types'
 
 // @unocss-include
@@ -28,9 +29,32 @@ const astroCompiler: Parser<typeof Astro, Astro.ParseOptions> = {
   getNodeLocation: genGetNodeLocation('positionOffset'),
 }
 
+const astroCompilerRs: Parser<typeof AstroRs> = {
+  id: 'astro-compiler-rs',
+  label: '@astrojs/compiler-rs',
+  icon: 'i-vscode-icons:file-type-light-astro',
+  link: 'https://github.com/withastro/compiler-rs',
+  editorLanguage: 'astro',
+  options: {
+    configurable: false,
+    defaultValue: {},
+    editorLanguage: 'json',
+  },
+  pkgName: '@astrojs/compiler-rs',
+  getModuleUrl: (_pkg, version) =>
+    `https://esm.sh/@astrojs/compiler-binding-wasm32-wasi${
+      version ? `@${version}` : ''
+    }/astro.wasi-browser.js`,
+  parse(code) {
+    const result = this.parseAstroSync(code)
+    return { ast: JSON.parse(result.ast), diagnostics: result.diagnostics }
+  },
+  getNodeLocation: genGetNodeLocation('startEnd'),
+}
+
 export const astro: LanguageOption = {
   label: 'Astro',
   icon: 'i-vscode-icons:file-type-light-astro',
-  parsers: [astroCompiler],
+  parsers: [astroCompiler, astroCompilerRs],
   codeTemplate: astroTemplate,
 }
