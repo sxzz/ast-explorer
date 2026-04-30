7722ec2a624359f3c3953b8d54b0c17fa71e1fe0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-30 14:00:36 -0400

    gjs <-> gts split

--- app/parser/ember.ts
+++ app/parser/ember.ts
@@ -1,22 +1,25 @@
 import { emberTemplate } from './template'
 import type { LanguageOption, Parser } from './index'
 
-export const emberEstree: Parser<typeof Espree, Espree.Options> = {
-  id: 'ember-estree',
-  label: 'ember-estree',
-  // @unocss-include
+export const emberEstreeGJS: Parser<typeof Espree, Espree.Options> = {
+  id: 'ember-estree-gjs',
+  label: 'ember-estree (GJS)',
   icon: 'i-vscode-icons:file-type-ember',
   link: 'https://github.com/emberjs/ember.js',
   options: {
     configurable: false,
+    defaultValue: {},
     editorLanguage: 'json',
   },
   pkgName: 'ember-estree',
-  getModuleUrl: (pkg) => `https://esm.sh/${pkg}@0.6.3?bundle&target=esnext`,
+  getModuleUrl: (pkg) =>
+    `https://esm.sh/${pkg}@0.6.3?bundle&target=esnext&exports=parse`,
   parse(code, options) {
-    const result = this.parse(code, { ...options })
-
-    console.log(result)
+    const result = this.parse(code, {
+      ...options,
+      filePath: 'ast-explorer.gjs',
+      templateOnly: false,
+    })
 
     return result.program
   },
@@ -24,9 +27,35 @@ export const emberEstree: Parser<typeof Espree, Espree.Options> = {
   getNodeLocation,
 }
 
+export const emberEstreeGTS: Parser<typeof Espree, Espree.Options> = {
+  id: 'ember-estree-gts',
+  label: 'ember-estree (GTS)',
+  icon: 'i-vscode-icons:file-type-ember',
+  link: 'https://github.com/emberjs/ember.js',
+  options: {
+    configurable: false,
+    defaultValue: {},
+    editorLanguage: 'json',
+  },
+  pkgName: 'ember-estree',
+  getModuleUrl: (pkg) =>
+    `https://esm.sh/${pkg}@0.6.3?bundle&target=esnext&exports=parse`,
+  parse(code, options) {
+    const result = this.parse(code, {
+      ...options,
+      filePath: 'ast-explorer.gts',
+      templateOnly: false,
+    })
+
+    return result.program
+  },
+  editorLanguage: 'glimmer-ts',
+  getNodeLocation,
+}
+
 export const ember: LanguageOption = {
   label: 'Ember',
   icon: 'i-vscode-icons:file-type-ember',
-  parsers: [emberEstree],
+  parsers: [emberEstreeGJS, emberEstreeGTS],
   codeTemplate: emberTemplate,
 }
