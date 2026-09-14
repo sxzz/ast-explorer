544f835b21adf02848f05de8ed0d68ece17ae1f6
@NullVoxPopuli's reduced-access machine account for AI usage <268630448+NullVoxPopuli-ai-agent@users.noreply.github.com>
2026-09-14 01:05:14 -0400

    fix(ember): load ember-estree from esm.sh without bundling (#299)
    Co-authored-by: NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>

--- app/parser/ember.ts
+++ app/parser/ember.ts
@@ -17,7 +17,7 @@ export const emberEstreeGJS: Parser<typeof EmberESTree, ParseOptions> = {
   },
   pkgName: 'ember-estree',
   getModuleUrl: (pkgId) =>
-    `https://esm.sh/${pkgId}?bundle&target=esnext&exports=parse`,
+    `https://esm.sh/${pkgId}?target=esnext&exports=parse`,
   parse(code, options) {
     const result = this.parse(code, {
       ...options,
@@ -43,7 +43,7 @@ export const emberEstreeGTS: Parser<typeof EmberESTree, ParseOptions> = {
   },
   pkgName: 'ember-estree',
   getModuleUrl: (pkgId) =>
-    `https://esm.sh/${pkgId}?bundle&target=esnext&exports=parse`,
+    `https://esm.sh/${pkgId}?target=esnext&exports=parse`,
   parse(code, options) {
     const result = this.parse(code, {
       ...options,
